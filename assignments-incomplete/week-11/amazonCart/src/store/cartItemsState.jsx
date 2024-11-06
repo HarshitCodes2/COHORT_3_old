bc76f422f925ec091136cc6b57a9302a0c047009
HarshitCodes2 <g1harshit7@gmail.com>
2024-11-06 12:48:02 +0530

    Halfway completed week 11 assignment - 2

--- assignments-incomplete/week-11/amazonCart/src/store/cartItemsState.jsx
+++ assignments-incomplete/week-11/amazonCart/src/store/cartItemsState.jsx
@@ -2,6 +2,13 @@ import { atom } from 'recoil';
 
 export const cartItems = atom({
   key: 'cartItems',
-  default: [9, 8, 7, 6]  // Will only store object of id of Items with qty.
+  default: [9, 8, 7, 6],  // Will only store object of id of Items with qty.
+  effects: [
+    ({onSet}) => {
+      onSet((id) => {
+        console.log("Current user ID: " + id);
+      });
+    },
+  ],
 });
 
--- assignments-incomplete/week-11/amazonCart/src/store/wishListItemsState.jsx
+++ assignments-incomplete/week-11/amazonCart/src/store/wishListItemsState.jsx
@@ -2,5 +2,12 @@ import { atom } from 'recoil';
 
 export const wishlistItems = atom({
   key: 'wishlistItems',
-  default: [1, 2, 3, 4]  // Will only store id of Items
+  default: [1, 2, 3, 4],  // Will only store id of Items
+  effects: [
+    ({onSet}) => {
+      onSet((id) => {
+        console.log("Current user ID: " + id);
+      });
+    },
+  ],
 });
